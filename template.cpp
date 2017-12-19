89d0d98d7d6ac5fcc4391ea69e229b33b49be562
Suyash Agrawal <ozym4nd145@outlook.com>
2017-12-19 15:15:59 +0530

    Added template for c++

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PII pair<int,int>
+#define LL long long int
+#define PLL pair<LL,LL>
+#define MK make_pair
+#define PB push_back
+#define SZ(a) ((int)(a.size()))
+#define MOD(a,m) ( ( ( (a) % (m) ) + (m) ) % (m) )
+
+//Debugging macros
+#define what_is(x) cerr << #x << " is " << x << endl;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
